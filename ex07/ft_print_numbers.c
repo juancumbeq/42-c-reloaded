0fb989a1e8438805d44e9ec2239a5283f0d8017c
juancumbeq <juanfernandocumbe@gmail.com>
2024-06-13 18:30:34 +0200

    update42

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcumbe-q <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 17:10:38 by jcumbe-q          #+#    #+#             */
+/*   Updated: 2024/06/10 17:13:17 by jcumbe-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ * Create a function that displays all digits, on a single line, by ascending order.
+ *
+ * Here's how it should be prototyped: void ft_print_numbers(void)
+ */
